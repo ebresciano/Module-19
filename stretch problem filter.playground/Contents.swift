925b4369f3fabcf92c144a75ff5ce605d2976dde
Eva <Bresciano>
2016-06-02 09:51:44 -0600

    h

--- stretch problem filter.playground/Contents.swift
+++ stretch problem filter.playground/Contents.swift
@@ -5,14 +5,30 @@ import UIKit
 var str = "Hello, playground"
 
 
-var array = [11, 23, 43, 44, 59, 88, 92, 58, 11, 13, 14, 15]
+var arrayA = [11, 23, 43, 44, 59, 88, 92, 58, 11, 13, 14, 15]
 
-array = array.filter {$0 % 3 == 0}
+let array = arrayA.filter {$0 % 3 == 0}
 
 print(array)
 
-let highNumber = array.reduce(0, combine: +)
+let reducedNumber = arrayA.reduce(0) { (a, b) -> Int in
+    if a > b {
+    return a
+    }
+    else { return
+    b
+    }
+}
+
+print(reducedNumber)
+
 
 var heart = ["I", "Love", "DevMountain "]
 
 let love = heart.reduce("", combine: +)
+
+let love2 = heart.reduce("", combine: {$0 + " " + $1})
+
+print(love2)
+
+//3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75
